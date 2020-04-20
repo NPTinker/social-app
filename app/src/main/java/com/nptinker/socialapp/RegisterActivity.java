3afc348f017e0507c8e635f7b3d5a101eed581fb
NPTinker <nghiaphampersonal@gmail.com>
2020-04-20 10:34:54 +0700

    Initial commit

---
+++ app/src/main/java/com/nptinker/socialapp/RegisterActivity.java
@@ -0,0 +1,113 @@
+package com.nptinker.socialapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+    EditText mEtEmail, mEtPassword;
+    Button mBtnRegister;
+    ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
+    //ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //Set actionbar and the title
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Create an account");
+        //Enable back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        //initialize stuffs
+        mBtnRegister = findViewById(R.id.btn_register2);
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPassword = findViewById(R.id.et_password);
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering...");
+
+        //Click event for register button
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEtEmail.getText().toString().trim();
+                String password = mEtPassword.getText().toString().trim();
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEtEmail.setError("Invalid Email");
+                    mEtEmail.setFocusable(true);
+                } else if (password.length()<6){
+                    mEtPassword.setError("Password at least 6 chars");
+                    mEtPassword.setFocusable(true);
+                } else registerUser(email, password);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed(); //go previous activity
+        return super.onSupportNavigateUp();
+    }
+
+    private void registerUser(String email, String password) {
+        //progressDialog.show();
+        //Create a new createAccount method which takes in an email address and password,
+        //validates them and then creates a new user with the createUserWithEmailAndPassword method.
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "start creating user");
+                        if (task.isSuccessful()) {
+                            // Sign in success, dismiss dialog and start register activity
+                            //progressDialog.dismiss();
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Success Registration",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            finish(); //end activity lifecycle
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //progressDialog.dismiss();
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, ""+e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
