27d11fc4da654cb42e04655d7fd0dc71ae8c506c
NPTinker <nghiaphampersonal@gmail.com>
2020-04-22 21:33:29 +0700

    Show user profile

--- app/src/main/java/com/nptinker/socialapp/ProfileFragment.java
+++ app/src/main/java/com/nptinker/socialapp/ProfileFragment.java
@@ -2,11 +2,26 @@ package com.nptinker.socialapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -14,15 +29,68 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    private ImageView mIvAvatar;
+    TextView mTvName, mTvEmail, mTvPhone;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //inflate layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getInstance().getReference("Users");
+
+        //initialize views
+        mIvAvatar = view.findViewById(R.id.iv_avatar);
+        mTvName = view.findViewById(R.id.tv_name);
+        mTvEmail = view.findViewById(R.id.tv_email);
+        mTvPhone = view.findViewById(R.id.tv_phone);
+
+        //get info of current user
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //get data
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String phone = "" + ds.child("phone").getValue();
+                    String image = "" + ds.child("image").getValue(); // gonna change
+
+                    //Set data
+                    mTvName.setText(name);
+                    mTvEmail.setText(email);
+                    mTvPhone.setText(phone);
+                    try {
+                        //if image is received
+                        Picasso.get().load(image).into(mIvAvatar);
+                    } catch (Exception e){
+                        //any exception = set default
+                        Picasso.get().load(R.drawable.ic_add_image).into(mIvAvatar);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
     }
 }
