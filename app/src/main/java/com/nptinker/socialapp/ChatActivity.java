284f4e56bf1d2fd2c0da962fa2ce9f888dc2b8cf
NPTinker <nghiaphampersonal@gmail.com>
2020-04-27 10:55:47 +0700

    Add send message function

--- app/src/main/java/com/nptinker/socialapp/ChatActivity.java
+++ app/src/main/java/com/nptinker/socialapp/ChatActivity.java
@@ -1,14 +1,33 @@
 package com.nptinker.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -17,6 +36,12 @@ public class ChatActivity extends AppCompatActivity {
     private TextView mTvName, mTvUserStatus;
     private EditText mEtMessage;
     private ImageButton mBtnSend;
+    private String hisUid;
+    private String myUid;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    DatabaseReference usersDbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +58,98 @@ public class ChatActivity extends AppCompatActivity {
         mTvUserStatus = findViewById(R.id.tv_user_status);
         mEtMessage = findViewById(R.id.et_message);
         mBtnSend = findViewById(R.id.btn_send);
+
+        //firebase init
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usersDbRef = firebaseDatabase.getReference("Users");
+
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("userUid");
+        //get myUid
+        checkUserStatus();
+
+        //Search to get other user info
+        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
+        //get images and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    String name ="" + ds.child("name").getValue();
+                    String image ="" + ds.child("image").getValue();
+
+                    mTvName.setText(name);
+                    try{
+                        Picasso.get().load(image).into(mIvProfile);
+                    } catch (Exception e) {
+                        //set default image if there is any exception
+                        Picasso.get().load(R.drawable.ic_default_img).into(mIvProfile);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mEtMessage.getText().toString().trim();
+                //check if empty
+                if(TextUtils.isEmpty(message)){
+                    Toast.makeText(ChatActivity.this, "Empty message", Toast.LENGTH_SHORT).show();
+                } else {
+                    sendMessage(message);
+                    Toast.makeText(ChatActivity.this, "Sent message", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        // create "Chats" in database
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message", message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //reset input field
+        mEtMessage.setText("");
+    }
+
+    private void checkUserStatus(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user !=null){
+            //mTvProfile.setText(user.getEmail());
+            myUid = user.getUid(); //current user's uid
+        } else {
+            //move to MainActivity if not logged in
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //hide searchview
+        menu.findItem(R.id.action_search).setVisible(false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
