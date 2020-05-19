65ce9910a7eaacd3ff3318e47991d5fe9ac135e9
NPTinker <nghiaphampersonal@gmail.com>
2020-05-19 14:27:46 +0700

    Add showing post fuction and searchign function

--- app/src/main/java/com/nptinker/socialapp/HomeFragment.java
+++ app/src/main/java/com/nptinker/socialapp/HomeFragment.java
@@ -5,17 +5,33 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.nptinker.socialapp.adapters.AdapterPosts;
+import com.nptinker.socialapp.models.ModelPost;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,7 +39,11 @@ import com.google.firebase.auth.FirebaseUser;
  */
 public class HomeFragment extends Fragment {
 
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
+
+    private RecyclerView recyclerView;
+    private List<ModelPost> postList;
+    AdapterPosts adapterPosts;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -36,7 +56,68 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         firebaseAuth = FirebaseAuth.getInstance();
+
+        recyclerView = view.findViewById(R.id.rv_posts);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //show newest post first
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        //set layout to recyclerview
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        postList = new ArrayList<>();
+        loadPosts();
         return view;
+    }
+
+    private void loadPosts() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        //get data from this reference
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+                    postList.add(modelPost);
+                    adapterPosts = new AdapterPosts(getActivity(),postList);
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchPosts(final String query) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        //get data from this reference
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    if(modelPost.getpTitle().toLowerCase().contains(query.toLowerCase()) ||
+                            modelPost.getpDescription().toLowerCase().contains(query.toLowerCase())){
+                        postList.add(modelPost);
+                    }
+                    adapterPosts = new AdapterPosts(getActivity(),postList);
+                    recyclerView.setAdapter(adapterPosts);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getActivity(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void checkUserStatus(){
@@ -60,6 +141,35 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_main, menu);
+
+        //Search view to search posts by post title/descr
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+
+        //search listener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //call when search btn pressed
+                if (!TextUtils.isEmpty(query)) {
+                    searchPosts(query);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //call when add any letter
+                if (!TextUtils.isEmpty(newText)) {
+                    searchPosts(newText);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+        });
         super.onCreateOptionsMenu(menu, inflater);
     }
 
---
+++ app/src/main/java/com/nptinker/socialapp/adapters/AdapterPosts.java
@@ -0,0 +1,143 @@
+package com.nptinker.socialapp.adapters;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nptinker.socialapp.R;
+import com.nptinker.socialapp.models.ModelPost;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
+
+    Context context;
+    List<ModelPost> postList;
+
+    public AdapterPosts(Context context, List<ModelPost> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate row_posts.xml
+        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, parent, false);
+
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        //get data
+        String uid = postList.get(position).getUid();
+        String uEmail = postList.get(position).getuEmail();
+        String uName = postList.get(position).getuName();
+        String uDp = postList.get(position).getuDp();
+        String pId = postList.get(position).getpId();
+        String pTitle = postList.get(position).getpTitle();
+        String pDescription = postList.get(position).getpDescription();
+        String pImage = postList.get(position).getpImage();
+        String pTimeStamp = postList.get(position).getpTime();
+
+        //convert time
+        Calendar calendar = Calendar.getInstance(Locale.getDefault());
+        calendar.setTimeInMillis(Long.parseLong(pTimeStamp));
+        String pTime = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
+
+        //set data
+        holder.tvUName.setText(uName);
+        holder.tvPTime.setText(pTime);
+        holder.tvPTitle.setText(pTitle);
+        holder.tvPDescription.setText(pDescription);
+        //set user dp
+        try {
+            Picasso.get().load(uDp).placeholder(R.drawable.ic_default_img).into(holder.ivUPicture);
+        } catch (Exception e) {
+
+        }
+
+        //set post image
+        if (pImage.equals("noImage")){
+            holder.ivPImage.setVisibility(View.GONE);
+        } else {
+            try {
+                Picasso.get().load(pImage).into(holder.ivPImage);
+            } catch (Exception e) {
+
+            }
+        }
+
+
+        //handle btn click
+        holder.btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        holder.btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        holder.btnComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        holder.btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+        ImageView ivUPicture, ivPImage;
+        TextView tvUName, tvPTime, tvPTitle, tvPDescription, tvPLikes;
+        ImageButton btnMore;
+        Button btnLike, btnComment, btnShare;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivUPicture = itemView.findViewById(R.id.iv_u_picture);
+            ivPImage = itemView.findViewById(R.id.iv_p_image);
+            tvUName = itemView.findViewById(R.id.tv_u_name);
+            tvPTime = itemView.findViewById(R.id.tv_p_time);
+            tvPTitle = itemView.findViewById(R.id.tv_p_title);
+            tvPDescription = itemView.findViewById(R.id.tv_p_description);
+            tvPLikes = itemView.findViewById(R.id.tv_p_likes);
+            btnMore = itemView.findViewById(R.id.btn_more);
+            btnLike = itemView.findViewById(R.id.btn_like);
+            btnComment = itemView.findViewById(R.id.btn_comment);
+            btnShare = itemView.findViewById(R.id.btn_share);
+
+        }
+    }
+}
