5bf0b327e149d65b5883ade58f2fc240e2262dd7
NPTinker <nghiaphampersonal@gmail.com>
2020-06-10 14:33:12 +0700

    Add share function (share post to other app to send, not share to your own profile)

--- app/src/main/java/com/nptinker/socialapp/adapters/AdapterPosts.java
+++ app/src/main/java/com/nptinker/socialapp/adapters/AdapterPosts.java
@@ -3,6 +3,9 @@ package com.nptinker.socialapp.adapters;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.text.format.DateFormat;
 import android.view.Gravity;
@@ -20,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -40,6 +44,8 @@ import com.nptinker.socialapp.TheirProfileActivity;
 import com.nptinker.socialapp.models.ModelPost;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -82,8 +88,8 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         String uName = postList.get(position).getuName();
         String uDp = postList.get(position).getuDp();
         final String pId = postList.get(position).getpId();
-        String pTitle = postList.get(position).getpTitle();
-        String pDescription = postList.get(position).getpDescription();
+        final String pTitle = postList.get(position).getpTitle();
+        final String pDescription = postList.get(position).getpDescription();
         final String pImage = postList.get(position).getpImage();
         String pTimeStamp = postList.get(position).getpTime();
         final String pLike = postList.get(position).getpLike();
@@ -92,7 +98,7 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         //convert time
         Calendar calendar = Calendar.getInstance(Locale.getDefault());
         calendar.setTimeInMillis(Long.parseLong(pTimeStamp));
-        String pTime = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
+        final String pTime = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
 
         //set data
         holder.tvUName.setText(uName);
@@ -177,7 +183,15 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
         holder.btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) holder.ivPImage.getDrawable();
+                if (bitmapDrawable == null){
+                    //post without image
+                    shareTextOnly(pTitle, pDescription);
+                } else {
+                    //post with image
+                    Bitmap bitmap = bitmapDrawable.getBitmap();
+                    shareImageAndText(pTitle,pDescription,bitmap);
+                }
             }
         });
 
@@ -190,6 +204,43 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
                 context.startActivity(intent);
             }
         });
+    }
+
+    private void shareImageAndText(String pTitle, String pDescription, Bitmap bitmap) {
+        String shareBody = pTitle + "\n" + pDescription;
+        Uri uri = saveImageToShare(bitmap);
+        Intent sIntent = new Intent(Intent.ACTION_SEND);
+        sIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        sIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+        sIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+        sIntent.setType("image/png");
+        context.startActivity(Intent.createChooser(sIntent,"Share Via"));
+    }
+
+    private Uri saveImageToShare(Bitmap bitmap) {
+        File imageFolder = new File(context.getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imageFolder.mkdirs();
+            File file = new File(imageFolder, "shared_image.png");
+            FileOutputStream stream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream);
+            stream.flush();
+            stream.close();
+            uri = FileProvider.getUriForFile(context, "com.nptinker.socialapp.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return uri;
+    }
+
+    private void shareTextOnly(String pTitle, String pDescription) {
+        String shareBody = pTitle + "\n" + pDescription;
+        Intent sIntent = new Intent(Intent.ACTION_SEND);
+        sIntent.setType("text/plain");
+        sIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here"); //in case of an email app
+        sIntent.putExtra(Intent.EXTRA_TEXT, shareBody); //text to share
+        context.startActivity(Intent.createChooser(sIntent, "Share via"));//message to show in share dialog
     }
 
     private void setLikes(final MyHolder holder, final String postKey) {
